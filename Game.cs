63ef92dbc06b847f75773a96233a5cdba7cc5bdc
TKOFFICE\sunjp <sunjp@mail.jj.cn>
2024-12-10 11:58:35 +0800

    Init

--- Game.cs
+++ Game.cs
@@ -1,12 +1,45 @@
-﻿using UnityEngine;
+﻿using Core;
+using Framework.Yggdrasil;
+using Framework.Yggdrasil.Services;
+using JetBrains.Annotations;
 
 namespace Game.Named
 {
-    public class Game : MonoBehaviour
+    public class GamePack : IGamePack
     {
-        private void Start()
+        public string Name => "Named";
+        public string Path => "Game/Named/";
+        public string Version => "0.1.0";
+        public string Icon => "";
+
+        public IGameService CreateGameService()
         {
-            Debug.Log("Game Started");
+            return Injector.Instance.Register<IGameService, GameService>();
+        }
+    }
+
+    [UsedImplicitly]
+    public class GameService : IGameService
+    {
+        private readonly ILoggerService m_logger;
+
+        [ServiceConstructor]
+        public GameService(ILoggerService logger)
+        {
+            m_logger = logger;
+        }
+
+        public void OnAdd()
+        {
+        }
+
+        public void OnRemove()
+        {
+        }
+
+        public void Run()
+        {
+            m_logger.Log("Game started");
         }
     }
 }
